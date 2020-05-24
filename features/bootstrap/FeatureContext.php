5cfdbb9188adb42f9331d494f228d118e3ff793c
yukihiro-kawabata <yukihirochasuke0630@gmail.com>
2020-05-24 23:00:41 +0900

    behatを試してみた

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,31 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @When :time 秒待つ
+     */
+    public function wait($time)
+    {
+        sleep($time);
+    }
+
+}
