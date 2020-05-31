05b6a29214b3cc03d333547c6c0cac1a368fb180
yukihiro-kawabata <yukihirochasuke0630@gmail.com>
2020-05-31 22:09:35 +0900

    デモ用のlaravelでログインできるか試してみた

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,6 +4,8 @@ use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
+use Behat\MinkExtension\Context\MinkContext;
+
 /**
  * Defines application features from the specific context.
  */
@@ -26,6 +28,16 @@ class FeatureContext implements Context
     public function wait($time)
     {
         sleep($time);
+    }
+
+    /**
+     * @When メールアドレスとパスワードを入力
+     */
+    public function inputEmailAndPassword()
+    {
+        // $MinkContext = new MinkContext();
+        // $MinkContext->fillField('email', 'info@example.com');
+        // $MinkContext->fillField('password', 'passpass');
     }
 
 }
---
+++ features/laravel_login.feature
@@ -0,0 +1,13 @@
+# language: ja
+フィーチャ: laravelログイン
+
+    シナリオ: laravelログイン
+        # 前提 "/login" を表示している
+        前提 ホームページを表示している
+        # かつ メールアドレスとパスワードを入力
+        かつ "email" フィールドに "info@example.com" と入力する
+             かつ "password" フィールドに "passpass" と入力する
+             かつ "submitBtn" ボタンをクリックする
+	    かつ 1 秒待つ
+        ならば "もしかして: logged" と表示されていること
+
